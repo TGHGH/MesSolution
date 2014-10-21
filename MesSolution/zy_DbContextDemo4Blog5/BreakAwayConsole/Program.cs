70639533dc03f9de1f6476b32e69fbd92626cf36
lianggui1983 <229042735@qq.com>
2014-10-21 22:40:51 +0800

    11

--- MesSolution/zy_DbContextDemo4Blog5/BreakAwayConsole/Program.cs
+++ MesSolution/zy_DbContextDemo4Blog5/BreakAwayConsole/Program.cs
@@ -104,7 +104,7 @@ namespace BreakAwayConsole
             //ValidateNewPerson();
             //ValidateDestination();
             //ValidatePropertyOnDemand();
-            //ValidateTrip();
+            ValidateTrip();
             //ValidateEverything();
 
 
