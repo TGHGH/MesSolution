c5021f48f02ad9902149ac0e13dbd9565a8e65db
lianggui1983 <229042735@qq.com>
2014-10-31 08:20:26 +0800

    11

---
+++ MesSolution/Core.Service/Privage/IUserGroupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Component.Tools;
+using System.Linq;
+using Core.Models;
+namespace Core.Service
+{
+    public interface IUserGroupService
+    {
+        OperationResult AddEntity(UserGroup userGroup);
+        OperationResult DeleteEntity(string key);
+        OperationResult FindEntity(string key);
+        OperationResult UpdateEntity(UserGroup userGroup);
+        IQueryable<UserGroup> UserGroups();
+    }
+}
---
+++ MesSolution/Core.Service/Privage/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Component.Tools;
+using System.Linq;
+using Core.Models;
+namespace Core.Service
+{
+    public interface IUserService
+    {
+        OperationResult AddEntity(User user);
+        OperationResult DeleteEntity(string key);
+        OperationResult FindEntity(string key);      
+        OperationResult UpdateEntity(User user);
+        IQueryable<User> Users();
+
+        OperationResult Login(LoginInfo2 loginInfo);
+    }
+}
