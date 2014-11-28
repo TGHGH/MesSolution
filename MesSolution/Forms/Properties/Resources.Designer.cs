9e943cda2748a742ab7321215a0e7c2f60709b76
lianggui1983 <229042735@qq.com>
2014-11-28 09:21:35 +0800

    11

--- MesSolution/Forms/Properties/Resources.Designer.cs
+++ MesSolution/Forms/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Forms.Properties {
+namespace Frms.Properties {
     using System;
     
     
