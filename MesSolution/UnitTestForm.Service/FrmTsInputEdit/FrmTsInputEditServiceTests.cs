e894b20b34f0db533a265d88ef6114b06f7796e7
lianggui1983 <229042735@qq.com>
2014-11-29 13:06:02 +0800

    11

---
+++ MesSolution/UnitTestForm.Service/FrmTsInputEdit/FrmTsInputEditServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frm.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Frm.Service.Tests
+{
+    [TestClass()]
+    public class FrmTsInputEditServiceTests
+    {
+        [TestMethod()]
+        public void ActionNGConfirm()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void TsErrorCauseEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetErrorCodeSeasonByGroup()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void SaveTs()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void TsCompleteCheck()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void TsCompleteConfirm()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
